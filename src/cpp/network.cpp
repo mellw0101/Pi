2c347106986e8531f20502c546cf2aa877bd63c6
mellw0101 <melwivensson@gmail.com>
2024-07-01 10:24:58 +0200

    Changes to be committed: 	modified:   src/cpp/network.cpp

--- src/cpp/network.cpp
+++ src/cpp/network.cpp
@@ -342,7 +342,7 @@ setupServer(int serverPort)
         return -1;
     }
 
-    if ((clientFd = accept(serverFd, (struct sockaddr *)&clientSockAddr, &clientSockAddrSize)) < 0)
+    if ((clientFd = accept(serverFd, (struct sockaddr *)&clientSockAddr, &clientSockAddrSize)) <= -1)
     {
         perror("accept");
         return -1;
